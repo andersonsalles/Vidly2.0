42566c9a1f3fe39fade8e8c0e50fc18dd6dd3d48
Anderson <andersonsalles@outlook.com>
2020-04-11 19:11:44 -0300

    add BirthDate on view

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -5,20 +5,30 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModel;
+using WebApplication1.Models;
 
 namespace Vidly.Controllers
 {
     public class CustomerController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customer
         public ActionResult Index()
         {
-            var custumers = new List<Customer>()
-            {
-                new Customer {Name = "Customer 1", Id = 1},
-                new Customer {Name = "Customer 2", Id = 2}
-
-            };
+            List<Customer> custumers = GetCustomers(); 
+                
+          
             var viewModel = new CustomerViewModel
             {
                 Custumer = custumers
@@ -26,15 +36,16 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
+        private List<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
         [Route("Details/{id}")]
         public ActionResult Details(int id)
         {
             if (id <= 0) throw new ArgumentOutOfRangeException(nameof(id));
-            var customer = new Customer
-            {
-                Name = id == 1 ? "Customer 1" : "Customer 2", Id = id
-            };
-
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             return View(customer);
         }
